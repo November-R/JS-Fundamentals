663fc6b13333566b1b29e504068e9ea646123a14
Grace Sarah Lumiti <lumitigrace2019@gmail.com>
2025-09-17 23:37:34 +0300

    Javascript with more variables declared and printed

---
+++ 1-multi_langauages.js
@@ -0,0 +1,9 @@
+//variable declarations
+let cLanguage = "C is fun";
+let pyLanguage = "Python is cool";
+let jsLanguage = "Javascript is amazing";
+
+//printing the variables to the console
+console.log(cLanguage);
+console.log(pyLanguage);
+console.log(jsLanguage);
