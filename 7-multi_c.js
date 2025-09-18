f85a3ab1c4e9d93c6da5ab9ed5fd5d0fed239005
Grace Sarah Lumiti <lumitigrace2019@gmail.com>
2025-09-19 02:39:05 +0300

    Restructuring the code such that the if statemnt is outside and the while loop is inside it, in order to log the missing occurrences message to the console when there is no argument or the argument is not a number.

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,16 +1,16 @@
+//This program takes a number from the command line argument and prints "C is fun" that many times. If the argument is missing or not a number, it prints "Missing number of occurrences".
 let userArgument = process.argv[2];
 
-let i = 0;
+if (userArgument == undefined || isNaN(userArgument)) {
+    console.log("Missing number of occurrences");
+} else {
+    let i = 0;
 
-while(i < userArgument){
+    while (i < parseInt(userArgument)) {
+        console.log("C is fun!");
 
-    if(isNaN(userArgument)) {
-        console.log("Missing number of occurrences");
-    } else {
-        console.log("C is fun");
+        i++;
     }
-
-    i++;
 }
 
 
