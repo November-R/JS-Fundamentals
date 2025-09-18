6e7ba977641a3fb631d90d5a2a38c5b850efdb5d
Grace Sarah Lumiti <lumitigrace2019@gmail.com>
2025-09-18 23:23:49 +0300

    This program prints multiple lines of text using a while loop.

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+//A program that prints multiple lines of text using a loop.
+let lineArray = ["C is fun", "Python is fun", "Javascript is amazing"];
+
+let i = 0;
+while (i < lineArray.length){
+    console.log(lineArray[i]);
+    i++;
+}
