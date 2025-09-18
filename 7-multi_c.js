34302935b54cd01cb5e395d966131d45feb6bd9e
Grace Sarah Lumiti <lumitigrace2019@gmail.com>
2025-09-19 02:19:57 +0300

    This program takes a number from the terminal/command line and prints c is fun that many times. I fteh argumnet is missing or not a number, it prints missung number of occurences to the console.

---
+++ 7-multi_c.js
@@ -0,0 +1,24 @@
+let userArgument = process.argv[2];
+
+let i = 0;
+
+while(i < userArgument){
+
+    if(isNaN(userArgument)) {
+        console.log("Missing number of occurrences");
+    } else {
+        console.log("C is fun");
+    }
+
+    i++;
+}
+
+
+
+
+
+
+
+
+
+
