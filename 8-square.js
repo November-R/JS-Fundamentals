6b471a501e941a063d94eb7a4b7c5e89d9e0e910
Grace Sarah Lumiti <lumitigrace2019@gmail.com>
2025-09-19 02:59:27 +0300

    This program prints a square of a given size using the character --x--. The size is provided in the command line by the user. If no size is provided, it prints missing size.

---
+++ 8-square.js
@@ -0,0 +1,13 @@
+//This is a program that prints a square of a given size using the character 'x'. The size of the square is provided as a command line argument. If no size is provided, it prints "Missing size".
+let userArgument1 = process.argv[2];
+
+if (isNaN(userArgument1)) {
+    console.log("Missing size");
+} else {
+    let i = 0;
+
+    while (i < userArgument1) {
+        console.log("x".repeat(userArgument1));
+        i ++;
+    }
+}
