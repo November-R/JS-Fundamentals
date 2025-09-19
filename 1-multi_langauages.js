8f4f78a9bc4cc35ce0edab865d755d82adcba90a
Grace Sarah Lumiti <lumitigrace2019@gmail.com>
2025-09-19 04:02:47 +0300

    We are printing each line separetly using the console.log method.

---
+++ 1-multi_langauages.js
@@ -0,0 +1,10 @@
+//this program prints multiple lines of text to the console
+//variable declarations
+let cLanguage = "C is fun";
+let pyLanguage = "Python is cool";
+let jsLanguage = "JavaScript is amazing!";
+
+//printing the variables to the console
+console.log(cLanguage);
+console.log(pyLanguage);
+console.log(jsLanguage);
