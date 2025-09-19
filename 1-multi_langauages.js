06b7071065638fbb350be521891c11d832e16714
Grace Sarah Lumiti <lumitigrace2019@gmail.com>
2025-09-19 03:51:14 +0300

    We are making the console print all the text lines at once.

---
+++ 1-multi_langauages.js
@@ -0,0 +1,8 @@
+//this program prints multiple lines of text to the console
+//variable declarations
+let cLanguage = "C is fun";
+let pyLanguage = "Python is cool";
+let jsLanguage = "Javascript is amazing";
+
+//printing the variables to the console
+console.log(cLanguage, pyLanguage, jsLanguage);
