3dfafd6b25ee74a1b7abf06cb68f8ed16003677d
Grace Sarah Lumiti <lumitigrace2019@gmail.com>
2025-09-19 04:33:26 +0300

    we are changing Python is fun to Python is cool

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 //A program that prints multiple lines of text using a loop.
-let lineArray = ["C is fun", "Python is fun", "JavaScript is amazing"];
+let lineArray = ["C is fun", "Python is cool", "JavaScript is amazing"];
 
 let i = 0;
 while (i < lineArray.length){
